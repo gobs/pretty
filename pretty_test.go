cdb7ea451db8eb1065619bb8740e979a6da9fff1
Raffaele Sena <raff367@gmail.com>
2013-07-01 21:07:32 -0700

    Initial implementation

---
+++ pretty_test.go
@@ -0,0 +1,22 @@
+package pretty
+
+import "testing"
+
+type Bag map[string]interface{}
+
+func TestPrettyPrint(test *testing.T) {
+
+	bag := Bag {
+		"a": 1,
+		"b": false,
+		"c": "some stuff",
+		"d": 1.5,
+		"e": Bag {
+			"e1": "here",
+			"e2": []int { 1,2,3,4 },
+			"e3": nil,
+			},
+		}
+
+	PrettyPrint(bag)
+}
